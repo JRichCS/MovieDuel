7343e4c85478c3ef0d2b2ec640142bc66874ed3b
Austin <austin.mutti@gmail.com>
2025-04-11 12:57:14 -0400

    readded addCommentRoute

--- backend/server/server.js
+++ backend/server/server.js
@@ -6,19 +6,14 @@ const axios = require('axios');
 const userProfilePictureRoutes = require("./routes/userProfilePicture");
 
 const loginRoute = require('./routes/userLogin')
-const getAllUsersRoute = require('./routes/userGetAllUsers')
 const registerRoute = require('./routes/userSignUp')
 const getUserByIdRoute = require('./routes/userGetUserById')
 const dbConnection = require('./config/db.config')
-const editUser = require('./routes/userEditUser')
-const deleteUser = require('./routes/userDeleteAll')
 
 const favoritesRoute = require("./routes/userFavorites");
 const movieRoutes = require('./routes/movieRoutes');
 const gameScoreRoutes = require('./routes/gameScores'); // Adjust path if needed
 
-
-const favoritesRoute = require("./routes/userFavorites");
 const addcommentsRoute = require("./routes/userAddComment");
 const getcommentsRoute = require("./routes/userGetComment");
 const deletecommentRoute = require("./routes/userDeleteComment");
@@ -34,7 +29,6 @@ app.use(express.json());
 app.use('/user', loginRoute);
 app.use('/user', registerRoute);
 app.use('/user', getUserByIdRoute);
-app.use('/user', deleteUser);
 app.use("/user", favoritesRoute);
 app.use('/api', movieRoutes);
 app.use("/api/game", gameScoreRoutes);
